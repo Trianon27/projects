1d7e4c5bd150f3fd6dd7839a76cc19b825fbc121
Trianon27 <57604858+Trianon27@users.noreply.github.com>
2023-05-09 10:22:18 -0500

    Update sketch.js with slider

--- photomosaic/sketch.js
+++ photomosaic/sketch.js
@@ -1,13 +1,14 @@
 let img;
 let smallImage;
-let scl = 16;
+let scl =20;
 let images = [];
 let brightness = [];
 let brightnessImages = [];
 
+
 function preload() {
-  img = loadImage('assets/shitzu_lindo.jpg');
-    for (let x = 0; x <= 10; x++){
+  img = loadImage('https://upload.wikimedia.org/wikipedia/commons/3/30/Shih-Tzu.JPG');
+    /*for (let x = 0; x <= 10; x++){
       images[x] = loadImage('assets/' + str(x) + '.jpg');
       let avg = 0;
       for(let i = 0; i < images[x].width; i++){
@@ -17,20 +18,25 @@ function preload() {
     };
     avg = avg/ (images[x].width * images[x].height);
     brightness[x] = avg;
-  };
-};
+  };*/
+}
 
 function setup() {
+  p_slider = createSlider(1,100,1)
   createCanvas(400, 400);
   img.resize(400, 400);
   brightness = [256];
-  w = img.width/scl;
-  h = img.height/scl;
-  smallImage = createImage(w, h, RGB);
-  smallImage.copy(img, 0, 0, img.width, img.height, 0, 0, w, h);
+  
 }
 
 function draw(){
+  /*redimension values*/
+  scl = p_slider.value() 
+  w = int(img.width/scl);
+  h = int(img.height/scl);
+  smallImage = createImage(w, h);
+  smallImage.copy(img, 0, 0, img.width, img.height, 0, 0, w, h);
+  
   /* photomosaic */
   smallImage.loadPixels();
   for (let i = 0; i < w; i++){
@@ -39,7 +45,7 @@ function draw(){
       let r = smallImage.pixels[index];
       let g = smallImage.pixels[index + 1];
       let b = smallImage.pixels[index + 2];
-      let l = (r + g + b)/0.001;
+      let l = (r + g + b)/0.0000001;
       let col = color(r, g, b, l);
       fill(col);
       noStroke();
@@ -47,6 +53,6 @@ function draw(){
     }
   }
   
-  noLoop();
+
 
 }
