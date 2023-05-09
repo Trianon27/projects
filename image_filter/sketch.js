098f8a75d0c09cac57197d55b7345876b724f3c1
Trianon27 <srodriguezt@unal.edu.co>
2023-05-08 19:11:08 -0500

    third_post_effects

--- image_filter/sketch.js
+++ image_filter/sketch.js
@@ -3,37 +3,34 @@ let canvas;
 
 function preload(){
   img = loadImage('assets/shitzu_lindo.jpg');
+
 }
 
 function setup() {
-  createCanvas(img.width, img.height);
- 
+  createCanvas(400, 400);
+  img.resize(400, 400);
+  noLoop();
 
 }
 
 function draw() {
-  background(220);
+
   image(img, 0, 0); 
-  let newCanvasX = (windowWidth - img.width)/2;
-  let newCanvasY = (windowHeight - img.height)/2;
-  canvas.position(newCanvasX, newCanvasY);
 
-  for(let col = 0; col < img.width; col++){
-    for(let row = 0; row < img.height; row++){
-      let xPos  = col;
-      let yPos = row;
+    for(let col = 0; col < img.width; col++){
+      for(let row = 0; row < img.height; row++){
+        let xPos  = random(col);
+        let yPos = random(row);
 
-      let c = img.get(xPos, yPos);
-      push(c);
-      translate(Xpos, yPos);
-      rotate(radians(random(360)));
-      nonfill(c);
-      strokeWeight(random(5));
-      point(xPos, yPos);
-      strokeWeight(random(3));
-      stroke(color(c));      
-      curve(xPos, yPos, random(20), yPos, xPos, sin(yPos), xPos, cos(yPos));
-      pop();
+        let c = img.get(xPos, yPos);
+        /* rotate(radians(random(360))); */
+        noFill(c);
+        strokeWeight(random(5));
+        strokeWeight(random(3));
+        stroke(color(c));      
+        curve(xPos + random(160), yPos + random(90), xPos  - random(90), yPos - + random(160), 
+        xPos + + random(160), yPos + + random(90), xPos - + random(90), yPos - + random(160));
+      }
     }
-  }
 }
+
